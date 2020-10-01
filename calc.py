897c6f9aed9d1dd94ead44db1b29f62b584eedc8
yash5015 <70716232+yash5015@users.noreply.github.com>
2020-10-01 21:09:43 +0530

    update in Print statement

--- calc.py
+++ calc.py
@@ -1,21 +1,21 @@
 num1 = float(input("Enter First value : "))
-op = input("Choose operator : ")
+op = input("Choose operator : '+','-','*','/' ")
 num2 = float(input("Enter Second value : "))
 
 if op == '+' : 
     ans = num1 + num2
-    print("Result is : ", ans)
+    print("Sum is : ", ans)
 elif op == '-' : 
     ans = num1 - num2
-    print("Result is : ", ans)
+    print("Difference is : ", ans)
 elif op == '*' : 
     ans = num1 * num2
-    print("Result is : ", ans)
+    print("product is : ", ans)
 elif op == '/' : 
     ans = num1 / num2
     if num2 == 0 :
         print("Denominator should not be 0")
     else:
-        print("Result is : ", ans)
+        print("Quotient is : ", ans)
 else:
-    print("invalid inputs!")
+    print("Invalid Input!,")
